d30bdf7652809e4fa27ba2267f7e4c3496efc1cf
Rajkumar <mandarajkumar2001>
2026-06-02 11:36:52 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+# 1/bin/bash
+
+PRESON1=$1
+PRESON2=$2
+
+
+echo "$PRESON1:: Hey $PRESON2, How are you?"
+echo "$PRESON2:: Hello $PRESON1, I am fine. How are you doing"
+echo "$PRESON1: I am fine too. What's up?"
+echo "$PRESON2:: Nohting, just going to Mars now, will you come?"
+echo "$PRESON1:: Sorry, you carry on! I will come once you come back"
