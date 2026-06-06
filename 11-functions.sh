77efa89da5bd243607b645445107cd12204b02e6
Rajkumar <mandarajkumar2001>
2026-06-06 14:59:22 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -28,4 +28,15 @@ then
    VALIDATE $? "MYSQL"
 else 
     echo "MySQL is already installed...Nothing to do"
+
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then 
+    echo "python3 is not installed... going to install it"
+    dnf install python3 -y 
+    VALIDATE $? "PYTHON3"
+else
+    echo "python3 is already installed...Nothing to do"
 fi
