b3ff5cba597de55ed334420871ca907c65545fcc
Rajkumar <mandarajkumar2001>
2026-06-06 14:42:46 +0530

    instal-command

--- 10-instal-command.sh
+++ 10-instal-command.sh
@@ -3,10 +3,10 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR:: Please run this script with root success"
+    echo "ERROR:: Please run this script with root access"
     exit 1
 else
-     echo "You are running with root success"
+     echo "You are running with root access"
 fi
 
 dnf list installed mysql
