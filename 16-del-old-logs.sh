8e2a5a3f347735ed7eb8ebf1208c740a06b25afb
Rajkumar <mandarajkumar2001>
2026-07-28 12:07:42 +0530

    nginx

--- 16-del-old-logs.sh
+++ 16-del-old-logs.sh
@@ -21,16 +21,8 @@ else
     echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-# validate functions takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
+
+
 
 echo "Script started executing at $(date)" | tee -a $LOG_FILE
 
@@ -42,4 +34,4 @@ do
     rm -rf $filepath
 done <<< $FILES_TO_DELETE
 
-echo "Script executed successfully"
+echo "Script executed successfully" -a $LOG_FILE
