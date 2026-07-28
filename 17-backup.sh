5a3f6a27428330f603bf4aed5d77b5bb09f283b1
Rajkumar <mandarajkumar2001>
2026-07-28 13:26:03 +0530

    nginx

---
+++ 17-backup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+USERID=$(id -u)
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/backup.log"
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+check_root(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R ERROR:: Please run this script with root access $N"
+        exit 1 #give other than 0 upto 127
+    else
+        echo "You are running with root access" 
+    fi
+}
+check_root
+mkdir -p $LOGS_FOLDER
+
+USAGE(){
+    echo -e "$R USAGE:: $N sh 20-backup.sh <source-dir> <destination-dir> <days(optional)>"
+    exit 1
+}
+if [ $# -lt 2 ]
+then
+    USAGE
+fi
+
